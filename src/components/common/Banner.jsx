d3ecf7f7c8004a0cecb47f4eb6ac82d0dadaa7ca
loveneesh1804 <loveneeshpathak@Loveneeshs-MacBook-Pro.local>
2024-07-06 20:22:37 +0530

    Mobile Changes

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -3,6 +3,7 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import model1 from "../../assets/banner/model1.jpeg";
 import model2 from "../../assets/banner/model2.jpg";
 import model3 from "../../assets/banner/model3.png";
+import { useNavigate } from 'react-router-dom';
 
 
   
@@ -10,6 +11,7 @@ let i=1;
 const Banner = () => {
   const [right,setRight] = useState(0);
   const [count,setCount] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(()=>{
       var id = setInterval(()=>{
@@ -53,7 +55,7 @@ const Banner = () => {
               <div className="banner-content">
                 <h1>HAND <span style={{color:"red"}}>CRAFTED</span></h1>
                 <span>Exclusive Collection!</span>
-                <button>Shop Now<ArrowForwardIosIcon /></button>
+                <button onClick={()=>navigate('/shop')} >Shop Now<ArrowForwardIosIcon /></button>
               </div>
             </div>
             <div className="banner">
@@ -61,7 +63,7 @@ const Banner = () => {
               <div className='banner-content'>
                 <h1>New<span style={{color : "red"}}> Arrival</span></h1>
                 <span>Summer Neutrals</span>
-                <button>Shop Now<ArrowForwardIosIcon /></button>
+                <button onClick={()=>navigate('/shop')}>Shop Now<ArrowForwardIosIcon /></button>
               </div >
             </div>
             <div className="banner">
@@ -70,7 +72,7 @@ const Banner = () => {
                   <p>End of season SALE!</p>
                   <h3>Up to 50% off</h3>
                   <p style={{marginTop : "-10px"}}>Shop from 10,000+ styles</p>
-                  <div className="buttons">
+                  <div className="buttons" onClick={()=>navigate('/shop')}>
                       <button>Ladies</button>
                       <button>Men</button>
                       <button>Divided</button>
