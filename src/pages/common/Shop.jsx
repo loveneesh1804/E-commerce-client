86270643a730df7cbdce17a76626c2c817db3692
loveneesh1804 <loveneeshpathak@Loveneeshs-MacBook-Pro.local>
2024-07-06 20:42:12 +0530

    Display

--- src/pages/common/Shop.jsx
+++ src/pages/common/Shop.jsx
@@ -197,6 +197,7 @@ const Shop = () => {
                       className="main-product"
                       key={el._id}
                       onClick={()=>navigate(`/shop/${el._id}`)}
+                      style={{height : !filter && '527px'}}
                     >
 
                       <img
